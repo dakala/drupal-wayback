22a059800e1e62ca7feb823549591b95b82d16a1
Deji Akala <deji.akala@capgemini.com>
2025-09-10 10:34:09 +0100

    Add ddev commands. Update code to run

--- drupal5/sites/default/settings.php
+++ drupal5/sites/default/settings.php
@@ -89,7 +89,7 @@
  *   $db_url = 'mysqli://username:password@localhost/databasename';
  *   $db_url = 'pgsql://username:password@localhost/databasename';
  */
-$db_url = 'mysql://username:password@localhost/databasename';
+$db_url = 'mysql://db:db@db/db';
 $db_prefix = '';
 
 /**
